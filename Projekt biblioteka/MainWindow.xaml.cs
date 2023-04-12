0a4ddd1fbef94b55289e2312c996222b687acce5
Mikołaj Klekotka <HomikiGaming@gmail.com>
2023-04-12 16:00:10 +0200

    Dodaj pliki projektów.

---
+++ Projekt biblioteka/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Projekt_biblioteka
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        //Otwieranie nowego okna 
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 win2 = new Window2(this);
+            win2.Show();
+            
+        }
+
+
+
+        //Metoda dodajaca item w listbox
+        //public void UpdateListBox(Window2 window2)
+        //{
+        //    TextBox TextBoxItem = new TextBox();
+        //    List_Box.Items.Add(TextBoxItem);
+            
+        //}
+
+        public void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+           
+        }
+
+        private void ContentPresenter_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
+        }
+
+        private void Remove_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (list_Box.Items.Count > 0)
+                list_Box.Items.RemoveAt(list_Box.SelectedIndex);
+
+             List<string> lines = new List<string>();
+            using (StreamReader r = new StreamReader("E:/aPROJEKTY (studia)/Projekt biblioteka/Projekt biblioteka/bin/Debug/lista.txt"))
+            {
+                string line;
+                while ((line = r.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        //Automatyczne załadowanie pliku .txt
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            
+
+        }
+
+
+
+
+    }
+
+
+}
---
+++ Projekt biblioteka/Window2.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Projekt_biblioteka;
+using System.IO;
+using System.Diagnostics;
+
+namespace Projekt_biblioteka
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+
+        //private Window2 lstValue;
+
+        public Window2(MainWindow mainWindow)
+        {
+            InitializeComponent();
+
+
+        }
+
+
+
+        //Dodawanie do ListBoxa
+
+        public void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = Application.Current.MainWindow as MainWindow;
+            
+            if (string.IsNullOrEmpty(TitleBox.Text))
+               
+                return;
+            if (string.IsNullOrEmpty(PublishBox.Text))
+                return;
+
+            mainWindow.list_Box.Items.Add($"Tytuł: {TitleBox.Text} \nWydawnictwo: {PublishBox.Text}");
+
+            if (mainWindow.list_Box.Items.Count > 0)
+            {
+                using (TextWriter TW = new StreamWriter("lista.txt"))
+                {
+                    foreach (string itemText in mainWindow.list_Box.Items)
+                    {
+                        TW.WriteLine(itemText);
+                    }
+                }
+
+               // Process.Start("lista.txt");
+            }
+
+
+
+            this.Close();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
